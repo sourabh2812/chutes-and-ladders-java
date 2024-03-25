0057c02ac9d50eadce94526a3bc040126a6700ef
Sourabh Suman <sourabh2812@live.com>
2024-03-24 23:46:23 -0400

    Add files via upload

---
+++ src/test/java/com/priceline/chutes/BoardSquareTest.java
@@ -0,0 +1,13 @@
+package com.priceline.chutes;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BoardSquareTest {
+
+    @Test
+    public void aChuteShouldSkipSquaresReversed(){
+        BoardSquare square = new BoardSquare(true, false, 10);
+        assertTrue(square.getNumberSquaresToSkip() < 0);
+    }
+}
